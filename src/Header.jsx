10b3af79afa4ae2987954048b8a1419547857c33
Mustapha Anthonio <engamo123@gmail.com>
2023-10-17 18:11:00 +0100

    updated

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div>
+      <h1>Hello Header</h1>
+      <img src="assets" alt="" />
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Hero.jsx
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <div>
+      <h1>Hello hero</h1>
+      <img src="{logo}" alt="" />
+    </div>
+  )
+}
+
+export default Hero
