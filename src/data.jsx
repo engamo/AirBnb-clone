935edb36a4a79b9e06902344bb3224221113ac08
Mustapha Anthonio <engamo123@gmail.com>
2023-11-03 23:09:31 +0100

    updated

--- src/data.jsx
+++ src/data.jsx
@@ -4,7 +4,7 @@ export default [
       title: "Life Lessons with Katie Zaferes",
       description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
       price: 136,
-      coverImg: "Katie.png",
+      coverImg: "katie-zafarez.png",
       button: "SOLD OUT",
       stats: {
           rating: 5.0,
